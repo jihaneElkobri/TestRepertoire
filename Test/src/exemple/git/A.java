7faee885cbe857e728f2362d41b96c3eeb6e7ab7
JIHANE <JIHANE@JIHANE-PC>
2013-09-06 17:43:30 +0100

    1er projet

---
+++ Test/src/exemple/git/A.java
@@ -0,0 +1,14 @@
+package exemple.git;
+
+public class A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
